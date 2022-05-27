17a648952a9f43ae517e622ec241b6822fbf8ffe
Garfield-Finch <tuzhitz@163.com>
2022-05-27 14:41:53 -0700

    add FigherManager

--- Assets/Scripts/FighterManager.cs
+++ Assets/Scripts/FighterManager.cs
@@ -2,15 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fighter : MonoBehaviour
+public class FighterManager : MonoBehaviour
 {
-    public int health;
-    public int combo;
     // Start is called before the first frame update
     void Start()
     {
-        this.health = 100;
-        this.combo = 0;
         
     }
 
@@ -19,6 +15,4 @@ public class Fighter : MonoBehaviour
     {
         
     }
-
-
 }
