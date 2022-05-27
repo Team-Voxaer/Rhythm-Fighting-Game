a5dad57e065c774a8380645309865007afd75990
Garfield-Finch <tuzhitz@163.com>
2022-05-27 14:36:54 -0700

    add Fighter

---
+++ Assets/Scripts/Fighter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    public int health;
+    public int combo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.health = 100;
+        this.combo = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
