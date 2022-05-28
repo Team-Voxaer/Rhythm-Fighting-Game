d7b2688dd413789b2cf0194dabee80403cebb1d7
Garfield-Finch <tuzhitz@163.com>
2022-05-27 23:44:35 -0700

    add methods for Fighter

--- Assets/Scripts/Fighter.cs
+++ Assets/Scripts/Fighter.cs
@@ -1,16 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static CommonParameter;
+using Input;
 
 public class Fighter : MonoBehaviour
 {
     public int health;
-    public int combo;
+    int combo;
+    public bool isCollectSkill;
+    public int skillUse;
+    int skillHold;  // Currently we only support hold one single skill. Collecting new skill will override the skill in hold.
+
+    // status
+    public int debuff;
+
     // Start is called before the first frame update
-    void Start()
+    void Start(int isLeftOrRight)
     {
         this.health = 100;
         this.combo = 0;
+
+        this.isCollectSkill = 0;  // To be deleted
+
+        this.skillUse = CommonParameter.Skill.none;
+        this.skillHold = CommonParameter.Skill.none;
+
+        this.debuff = CommonParameter.Debuff.none;
+        this.buff = CommonParameter.Buff.none;
+
+        if (isLeft) {
+            this.KeyCode.Skill = CommonParameter.KeyCode.LeftSkill;
+        } else {
+            this.KeyCode.Skill = CommonParameter.KeyCode.RightSkill;
+        }
         
     }
 
@@ -20,5 +43,31 @@ public class Fighter : MonoBehaviour
         
     }
 
+    public void ApplyDebuff(int debuffValue) {
+        // TODO: to interact with the sound strack group
+        self.debuff = debuffValue;
 
+    }
+
+    public void ApplyBuff(int buffValue) {
+        // TODO: to interact with the sound strack group
+        self.buff = buffValue;
+
+    }
+
+    void CastSkill() {
+        // detect key value
+        if (Input.GetKeyDown(CommonParameter.Userkey.skill)) {
+            this.skillHold = this.skillUse;
+            this.skillUse = CommonParameter.Skill.none;
+            // TODO: this may cause skill collection failure.
+        }
+    }
+
+    void CollectSkill() {
+        // detect key value to replace this.isCollectSkill
+        if (this.combo > CommonParameter.comboThreshold && this.isCollectSkill) {
+            this.skillHold = CommonParameter.Skill.one;
+        }
+    }
 }
