4b2a87b350c3b4a451d8fad8e19ed057fc2671ee
Garfield-Finch <tuzhitz@163.com>
2022-05-27 23:44:11 -0700

    add FighterManager

--- Assets/Scripts/FighterManager.cs
+++ Assets/Scripts/FighterManager.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fighter;
 
 public class FighterManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerLeft = Fighter()
+        playerRight = Fighter()
     }
 
     // Update is called once per frame
