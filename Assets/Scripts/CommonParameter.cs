da1574473cb4a030ab80a10d1ccb226b7a1da767
Garfield-Finch <tuzhitz@163.com>
2022-05-27 23:44:21 -0700

    add CommonParameters

---
+++ Assets/Scripts/CommonParameter.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CommonParameter
+{
+    // skills and combo
+    public static int comboThreshold = 10;
+
+    
+
+    public static class Skill {
+        public static int none = 0;
+        public static int one = 1;
+    }
+
+    
+    public static class Debuff {
+        public static int none = 0;
+        public static int one = 1;
+    }
+
+    public static class Buff {
+        public static int none = 0;
+        public static int autoPlay = 1;
+    }
+
+    public static class KeyCode {
+        // TODO: to survey how to represent KeyCode
+        public static KeyCode LeftSkill = KeyCode.D;
+        public static KeyCode RightSkill = KeyCode.L;
+
+    }
+
+
+}
