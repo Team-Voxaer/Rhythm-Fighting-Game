8c5b0475908f046d9c46efbff3af4760867129e7
Zhian Li <zhianli@usc.edu>
2022-06-07 19:49:16 -0700

    Add: Hit Effect 10 Frame

--- Assets/Scripts/HitEffect.cs
+++ Assets/Scripts/HitEffect.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class HitEffect : MonoBehaviour
 {
     Renderer rend;
+    int effectTimer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        effectTimer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (effectTimer > 0){
+            effectTimer--;
+        }
+        else {
+            rend = GetComponent<Renderer>();
+            rend.material.SetColor("_Color", Color.white);
+        }
     }
 
     public void ChangeColor(){
         rend = GetComponent<Renderer>();
         
         rend.material.SetColor("_Color", Color.black);
-
+        effectTimer = 10;
         
     }
 }
